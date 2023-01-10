420303d83ccd70763cd7318ea8a2fda31db7cb88
Brynfor Morris <brynfor.morris@gmail.com>
2023-01-10 12:12:19 -0600

    commit tree 422d4c1d4c3b4fc9912e71bc25fbb31a7bc2af33

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+
+
+const employee = {
+    name: 'Jimothy',
+    streetAddress: '21 Kringlemas Lane'
+};
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    const tempObj = {...obj};
+    tempObj[key] = value;
+    return tempObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+     obj[key] = value;
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj,key){
+    const trimObj={...obj};
+    delete trimObj[key];
+    return trimObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key];
+    return obj;
+};
